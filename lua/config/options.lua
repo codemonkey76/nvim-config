5079e747e2909c99bc57cccf9f28fc72386b3cf4
Shane Poppleton <shane@bjja.com.au>
2026-03-17 12:57:57 +1000

    Replace with current nvim config

---
+++ lua/config/options.lua
@@ -0,0 +1,3 @@
+vim.g.lazyvim_php_lsp = "intelephense"
+vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>", { desc = "Close Buffer" })
+vim.keymap.set("n", "<leader>Q", "<cmd>bd!<cr>", { desc = "Force Close Buffer" })
